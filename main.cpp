235c4ae585c0fc13e6a01c2efced3319accce4b5
Ragab2010 <35271240+Ragab2010@users.noreply.github.com>
2023-10-29 18:38:32 +0200

    now_it_work_with_http://localhost:8080/

--- main.cpp
+++ main.cpp
@@ -1,62 +1,89 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <cstdlib>
 #include <cstring>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 
-int main() {
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrLen = sizeof(clientAddr);
+// Constants
+const int PORT = 8080;
+const int MAX_CONNECTIONS = 10;
 
-    // Create a socket
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0) {
-        std::cerr << "Error creating socket" << std::endl;
+// HTTP response
+const std::string RESPONSE = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+
+// Function to get the current time
+std::string get_current_time() {
+    auto now = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(now);
+    return std::ctime(&now_c);
+}
+
+// Function to handle an incoming client connection
+void handle_client(int client_socket) {
+    char buffer[1024] = {0};
+    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+
+    if (bytes_received < 0) {
+        std::cerr << "Error reading from socket" << std::endl;
+    } else {
+        // Respond to the client
+        send(client_socket, RESPONSE.c_str(), RESPONSE.length(), 0);
+    }
+
+    // Close the client socket
+    close(client_socket);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    // Create the server socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        std::cerr << "Error opening socket" << std::endl;
         return 1;
     }
 
-    // Set up server address
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(5050); // Port 8080
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    // Bind the server socket to a specific address and port
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    // Bind the socket
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr) < 0)) {
-        std::cerr << "Error binding socket" << std::endl;
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr) ) < 0) {
+        std::cerr << "Error binding to socket" << std::endl;
         return 1;
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 5) < 0) {
-        std::cerr << "Error listening for connections" << std::endl;
-        return 1;
-    }
-
-    std::cout << "Server is running on port 8080..." << std::endl;
+    listen(server_socket, MAX_CONNECTIONS);
+    std::cout << "Server listening on port " << PORT << std::endl;
 
     while (true) {
-        // Accept a client connection
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
-        if (clientSocket < 0) {
-            std::cerr << "Error accepting client connection" << std::endl;
-            continue;
+        // Accept incoming client connections
+        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+        if (client_socket < 0) {
+            std::cerr << "Error accepting connection" << std::endl;
+            return 1;
         }
 
-        // Send a simple HTML response
-        std::string response = "HTTP/1.1 200 OK\r\n";
-        response += "Content-Type: text/html\r\n\r\n";
-        response += "<html><body><h1>Hello, World!</h1></body></html>";
-
-        send(clientSocket, response.c_str(), response.size(), 0);
-
-        // Close the client socket
-        close(clientSocket);
+        // Handle the connection in a separate thread
+        std::thread(handle_client, client_socket).detach();
     }
 
     // Close the server socket
-    close(serverSocket);
+    close(server_socket);
 
     return 0;
 }
